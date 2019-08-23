a1db1f7dc6a042249dae523667a5d1221f0294f4
nianqin <nianqin.vip@gmail.com>
2019-08-23 11:28:20 +0800

    update

--- dist/index.aio.js
+++ dist/index.aio.js
@@ -1,7 +1,7 @@
 /*!
  * nq-tslib-base 0.0.1 (https://github.com/nianqin/nq-tslib-base)
  * API https://github.com/nianqin/nq-tslib-base/blob/master/doc/api.md
- * Copyright 2017-2019 nianqin. All Rights Reserved
+ * Copyright 2019-2019 nianqin. All Rights Reserved
  * Licensed under MIT (https://github.com/nianqin/nq-tslib-base/blob/master/LICENSE)
  */
 
