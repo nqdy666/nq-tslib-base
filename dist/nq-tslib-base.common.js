8129c6b4b6b3b7c24b85222316cf8d41a45995be
nianqin <nianqin.vip@gmail.com>
2019-08-23 17:48:18 +0800

    'release'

--- dist/nq-tslib-base.common.js
+++ dist/nq-tslib-base.common.js
@@ -1,5 +1,5 @@
 /*!
- * nq-tslib-base 0.0.2 (https://github.com/nqdy666/nq-tslib-base)
+ * nq-tslib-base 0.0.3 (https://github.com/nqdy666/nq-tslib-base)
  * API https://github.com/nqdy666/nq-tslib-base/blob/master/doc/api.md
  * Copyright 2019-2019 nqdy666. All Rights Reserved
  * Licensed under MIT (https://github.com/nqdy666/nq-tslib-base/blob/master/LICENSE)
