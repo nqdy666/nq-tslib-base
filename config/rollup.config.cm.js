12a21f377bd8bf1247c9c5f236055ac671c8c354
nianqin <nianqin.vip@gmail.com>
2019-08-23 13:33:47 +0800

    update

--- config/rollup.config.cm.js
+++ config/rollup.config.cm.js
@@ -1,20 +1,24 @@
 // rollup.config.js
 // commonjs
 var common = require('./rollup.js');
+var uglify = require('rollup-plugin-uglify');
+
+var prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     input: 'src/index.' + common.type,
     output: {
-        file: 'dist/' + common.name + '.common.js',
+        file: prod ? 'dist/' + common.name + '.common.min.js' : 'dist/' + common.name + '.common.js',
         format: 'cjs',
         // When export and export default are not used at the same time, set legacy to true.
         // legacy: true,
-        banner: common.banner,
+        banner: prod ? '' : common.banner,
     },
     plugins: [
         common.getCompiler({
             tsconfigOverride: { compilerOptions : { declaration: true, module: 'ES2015' } },
             useTsconfigDeclarationDir: true
-        })
+        }),
+        (prod && uglify.uglify())
     ]
 };
--- config/rollup.config.esm.js
+++ config/rollup.config.esm.js
@@ -1,17 +1,23 @@
 // rollup.config.js
 // ES output
 var common = require('./rollup.js');
+var uglify = require('rollup-plugin-uglify');
+
+var prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     input: 'src/index.' + common.type,
     output: {
-        file: 'dist/' + common.name + '.esm.js',
+        file: prod ? 'dist/' + common.name + '.esm.min.js' : 'dist/' + common.name + '.esm.js',
         format: 'esm',
         // When export and export default are not used at the same time, set legacy to true.
         // legacy: true,
-        banner: common.banner,
+        banner: prod ? '' : common.banner,
     },
     plugins: [
-        common.getCompiler()
+        common.getCompiler(),
+        // https://github.com/mishoo/UglifyJS2#note
+        // uglify-js only supports JavaScript (ECMAScript 5).
+        // (prod && uglify.uglify())
     ]
 };
