33d59c950136412fc225a1f30f360412a9caff7b
nianqin <nianqin.vip@gmail.com>
2019-08-23 11:30:00 +0800

    update

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,20 +1,32 @@
 // rollup.config.js
-// commonjs
+// umd
+var nodeResolve = require('rollup-plugin-node-resolve');
+var commonjs = require('rollup-plugin-commonjs');
+var uglify = require('rollup-plugin-uglify');
+
 var common = require('./rollup.js');
+
+var prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     input: 'src/index.' + common.type,
     output: {
-        file: 'dist/index.js',
-        format: 'cjs',
+        file: prod ? 'dist/index.aio.min.js' : 'dist/index.aio.js',
+        format: 'umd',
         // When export and export default are not used at the same time, set legacy to true.
         // legacy: true,
+        name: common.name,
         banner: common.banner,
     },
     plugins: [
-        common.getCompiler({
-            tsconfigOverride: { compilerOptions : { declaration: true, module: 'ES2015' } },
-            useTsconfigDeclarationDir: true
-        })
+        nodeResolve({
+            mainFields: ['module', 'main'],
+            extensions: [common.type === 'ts' ? '.ts' : '', '.js']
+        }),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        common.getCompiler(),
+        (prod && uglify.uglify())
     ]
 };
