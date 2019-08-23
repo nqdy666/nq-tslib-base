0f24a72fbf45cb98eba4113469ab5587308ec0a5
nianqin <nianqin.vip@gmail.com>
2019-08-23 17:37:30 +0800

    $npm_package_version

--- dist/nq-tslib-base.esm.js
+++ dist/nq-tslib-base.esm.js
@@ -1,8 +1,8 @@
 /*!
- * nq-tslib-base 0.0.1 (https://github.com/nianqin/nq-tslib-base)
- * API https://github.com/nianqin/nq-tslib-base/blob/master/doc/api.md
- * Copyright 2019-2019 nianqin. All Rights Reserved
- * Licensed under MIT (https://github.com/nianqin/nq-tslib-base/blob/master/LICENSE)
+ * nq-tslib-base 0.0.1 (https://github.com/nqdy666/nq-tslib-base)
+ * API https://github.com/nqdy666/nq-tslib-base/blob/master/doc/api.md
+ * Copyright 2019-2019 nqdy666. All Rights Reserved
+ * Licensed under MIT (https://github.com/nqdy666/nq-tslib-base/blob/master/LICENSE)
  */
 
 // plus a + b
