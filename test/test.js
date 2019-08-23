77af767d63f12e6213f3c88d0b6f8b894f9fdb66
nianqin <nianqin.vip@gmail.com>
2019-08-23 14:35:11 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,54 +1,14 @@
-
-const expect = require('expect.js');
-require('jsdom-global')();
-
-// js 测试源文件
-import QuickApp from '../src/index.js'
-
+var expect = require('expect.js');
 
 // ts 测试编译后文件
-// var base = require('../src/index.ts');
+var base = require('../src/index.ts');
 
+describe('单元测试', function () {
+  this.timeout(1000);
 
-
-describe('单元测试 开始测试快应用库', function() {
-    this.timeout(1000);
-
-    const quickApp1 = new QuickApp();
-
-    describe('打开跳转页', function() {
-        it('传入options为空', function() {
-            expect(quickApp1.open()).to.be(undefined);
-        });
-
-        it('传入options.path为空', function() {
-            expect(quickApp1.open( {path: ''} )).to.be(undefined);
-        });
-
-        it('传入options.path 缺少 / 开头', function() {
-            expect(quickApp1.open( {path: 'index'} )).to.be(undefined);
-        });
-
-        it('传入options.params 不是对象', function() {
-            expect(quickApp1.open( {path: '/index', params: 'aaa'} )).to.be(undefined);
-        });
-
-        it('传入options.openType 为 url 不带有 confirm', function() {
-            expect(quickApp1.open( {path: '/index', openType: 'url'} )).to.be(true);
-        });
-
-        it('传入options.openType 为 url 带有 confirm', function() {
-            expect(quickApp1.open( {path: '/index', openType: 'url', confirm: '提示信息'} )).to.be(true);
-        });
-
-        it('传入options.openType 为 deeplink', function() {
-            expect(quickApp1.open( {path: '/index', openType: 'deeplink'} )).to.be(true);
-        });
-
-        it('传入options.openType 为 空', function() {
-            expect(quickApp1.open( {path: '/index'} )).to.be(true);
-        });
+  describe('功能1', function () {
+    it('相等', function () {
+      expect(base.add(1, 1)).to.equal(2);
     });
-
-
+  });
 });
